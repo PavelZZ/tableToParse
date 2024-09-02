c0811f5b471d5e05e04e36cf79cc896e7cf3641d
Зуев Павел Валерьевич <zuev@eosplatforma.ru>
2024-09-02 10:56:01 +0500

    gh-pages

--- output-table/src/components/ui/InputSearch.tsx
+++ output-table/src/components/ui/InputSearch.tsx
@@ -4,7 +4,7 @@ import { type SearchProps } from 'antd/es/input';
 import { CloseOutlined } from '@ant-design/icons';
 
 // Строка поиска с крестиком для очищения
-function InputSearch(props: Omit<SearchProps, 'suffix'> & { onCrossClick: () => void }) {
-    return <Input.Search {...props} suffix={<CloseOutlined onClick={props.onCrossClick} />} />
+function InputSearch({onCrossClick, ...otherProps}: Omit<SearchProps, 'suffix'> & { onCrossClick: () => void }) {
+    return <Input.Search {...otherProps} suffix={<CloseOutlined onClick={onCrossClick} />} />
 }
 export default InputSearch
